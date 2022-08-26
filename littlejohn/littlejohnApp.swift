903844370a7a0f40e7ec3549ac67d7c91944ac32
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2022-08-25 17:23:14 -0700

    Initial Commit

---
+++ littlejohn/littlejohnApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlejohnApp.swift
+//  littlejohn
+//
+//  Created by Dante Gil-Marin on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct littlejohnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
